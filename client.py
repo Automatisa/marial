c82266f41b43b736d059a118c984471aac2bf313
mohamedouselati <mohamedouselati@gmail.com>
2012-10-16 19:19:21 +0000

    hello amine

--- client.py
+++ client.py
@@ -13,40 +13,62 @@
 #You should have received a copy of the GNU General Public License
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 import readline
+COMMANDS = ['exit', 'extra', 'extension', 'stuff', 'errors',
+            'email', 'foobar', 'foo','ls','pwd']
+import urllib,urllib2
+import commands
+from time import sleep
+def complete(text, state):
+    for cmd in COMMANDS:
+        if cmd.startswith(text):
+            if not state:
+                return cmd
+            else:
+                state -= 1
 
-class main:
-    def __init__(self):
-        #TODO: import commands from server
-        self.commands = ['ls', 'mv', 'cp'] 
-        self.command = ''
-    
-    def execute(self):
+section = ""
+readline.parse_and_bind("tab: complete")
+readline.set_completer(complete)
+while section != "exit":
+    section = raw_input('[station]: ')
+    #URL du formulaire
+    url ='http://localhost/util.php'
 
-        print 'i am client'
-        while self.command != 'exit':
-            self.command = raw_input('> ')
-        return
     
-    def complete(self, text, state):
-        print text
-        for cmd in self.commands:
-            if cmd.startswith(text):
-                if not state:
-                    return cmd
-                else:
-                    state -= 1
+    
+
+    #Champ et valeur du formulaire 
+    values={'action':'write','buf': 'cmd', 'msg':section}
+    params = urllib.urlencode(values)
+    
+    #Envoi de la requete
+    req = urllib2.Request(url, params)
+    
+    #Recuperation de la commande cote serveur
+    response2 = urllib2.urlopen(req)
+    data2=response2.read()
+    
+    
+    #Execution de la commande(pas encore faite ) et envoi le flux dans out.txt
+    s=commands.getoutput(data2)
+    values1={'action': 'write','buf': 'out','msg':s}
+    params1=urllib.urlencode(values1)
+    
+    #Envoi de la requete cote serveur
+    req1=urllib2.Request(url,params1)
+    
+    #Recuperation de flux cote client
+    sleep(15)
+    values2={'action': 'read','buf': 'out'}
+    response=urllib2.urlopen(req1)
+    data=response.read()
+    print data
+    
+    
 
 
-if __name__ == '__main__':        
-    instance = main()
-    
-    readline.parse_and_bind("tab: complete")
-    readline.set_completer(instance.complete)
-    
-    raw_input('> ')
-    
-    instance.execute()
-    
-    
+
+
+
+
